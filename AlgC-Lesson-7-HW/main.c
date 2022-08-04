92dce6fc39e64e8834ae09831d1eeaa32a41fdca
Mertensoid <matsoukm@yandex.ru>
2022-08-04 20:29:25 +0300

    Initial Commit

---
+++ AlgC-Lesson-7-HW/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AlgC-Lesson-7-HW
+//
+//  Created by admin on 04.08.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
